d2f4af133732a5f2ea63041243c875f4a9df5fbb
HM-Azijul <azijulhm@gmail.com>
2022-05-19 14:26:53 +0600

    handleInputFields code way-01

--- src/App.jsx
+++ src/App.jsx
@@ -15,14 +15,32 @@ const InitialInputState = {
 const App = () => {
   const [inputState, setInputeState] = useState({ ...InitialInputState });
 
+  const handleInputFields = (e) => {
+    // console.log(e.target.name);
+    setInputeState({
+      ...inputState,
+      [e.target.name]: parseInt(e.target.value),
+    });
+  };
+
   return (
     <div style={{ width: '50%', margin: '0 auto' }}>
       <h1>Result: 0</h1>
 
       <div>
         <p>Inputs</p>
-        <input type="number" value={inputState.a} />
-        <input type="number" value={inputState.b} />
+        <input
+          type="number"
+          onChange={handleInputFields}
+          value={inputState.a}
+          name="a"
+        />
+        <input
+          type="number"
+          onChange={handleInputFields}
+          value={inputState.b}
+          name="b"
+        />
       </div>
 
       <div>
